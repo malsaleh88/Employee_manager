b181de8fa8cd3cbf72f71efbbf5b0832e1b5b5a3
malsaleh <mohammad.alsaleh@uclouvain.be>
2023-08-14 14:46:44 +0200

    add addEmployee method

--- src/main/java/com/alsaleh/employeemanager/EmployeeResource.java
+++ src/main/java/com/alsaleh/employeemanager/EmployeeResource.java
@@ -5,10 +5,7 @@ import com.alsaleh.employeemanager.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,5 +33,11 @@ public class EmployeeResource {
     public ResponseEntity<Employee> getEmployeeById (@PathVariable("id") Long id) {
         Employee employee =employeeService.findEmployeeById(id);
         return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Employee> addEmployee (@RequestBody Employee employee) {
+        Employee newEmployee =employeeService.addEmployee(employee);
+        return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
     }
 }
