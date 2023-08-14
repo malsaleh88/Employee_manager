a4696fb305336eb1dd206fc14d63290ec600359a
malsaleh <mohammad.alsaleh@uclouvain.be>
2023-08-14 14:39:59 +0200

    add getEmployeeById method

--- src/main/java/com/alsaleh/employeemanager/EmployeeResource.java
+++ src/main/java/com/alsaleh/employeemanager/EmployeeResource.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,12 +24,16 @@ public class EmployeeResource {
     }
 
 
-    @GetMapping
+    @GetMapping("/all")
     public ResponseEntity<List<Employee>> getAllEmployees () {
         List<Employee> employees =employeeService.findAllEmployees();
         return new ResponseEntity<>(employees, HttpStatus.OK);
     }
 
-
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Employee> getEmployeeById (@PathVariable("id") Long id) {
+        Employee employee =employeeService.findEmployeeById(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
 
 }
