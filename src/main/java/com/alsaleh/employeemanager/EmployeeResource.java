10a3f947c5e59424f3cb80d2aee97e375d68aeb5
malsaleh <mohammad.alsaleh@uclouvain.be>
2023-08-14 14:41:04 +0200

    add getEmployeeById method

--- src/main/java/com/alsaleh/employeemanager/EmployeeResource.java
+++ src/main/java/com/alsaleh/employeemanager/EmployeeResource.java
@@ -30,10 +30,11 @@ public class EmployeeResource {
         return new ResponseEntity<>(employees, HttpStatus.OK);
     }
 
+
+
     @GetMapping("/find/{id}")
     public ResponseEntity<Employee> getEmployeeById (@PathVariable("id") Long id) {
         Employee employee =employeeService.findEmployeeById(id);
         return new ResponseEntity<>(employee, HttpStatus.OK);
     }
-
 }
