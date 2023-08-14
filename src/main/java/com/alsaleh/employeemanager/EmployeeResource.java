4879e0f05733a6ff643cf6fe214a9597d5ffb2ec
malsaleh <mohammad.alsaleh@uclouvain.be>
2023-08-14 12:51:19 +0200

    add getAllEmployees method

--- src/main/java/com/alsaleh/employeemanager/EmployeeResource.java
+++ src/main/java/com/alsaleh/employeemanager/EmployeeResource.java
@@ -1,9 +1,15 @@
 package com.alsaleh.employeemanager;
 
+import com.alsaleh.employeemanager.model.Employee;
 import com.alsaleh.employeemanager.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/employee")
@@ -11,10 +17,18 @@ public class EmployeeResource {
 
     private final EmployeeService employeeService;
 
-
     @Autowired
     public EmployeeResource(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
+
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees () {
+        List<Employee> employees =employeeService.findAllEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+
 
 }
