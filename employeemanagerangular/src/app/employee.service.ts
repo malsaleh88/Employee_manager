13bd58ad74e16b967d0176000f4dc6f1f232daea
malsaleh <mohammad.alsaleh@uclouvain.be>
2023-08-16 15:44:20 +0200

    create addEmployee method

--- employeemanagerangular/src/app/employee.service.ts
+++ employeemanagerangular/src/app/employee.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http'; // Don't forget to import HttpClient
+import { Employee } from './employee';
 
 
 @Injectable({
@@ -12,8 +13,14 @@ export class EmployeeService{
     private apiServerUrl ='';
 
     constructor(private http: HttpClient) {}
+
     public getEmployees(): Observable<any> {
-        return this.http.get<any>('${this.apiServerUrl}/employee/all')
+        return this.http.get<any>('${this.apiServerUrl}/employee/all');
+        
+    }
+
+    public addEmployee(employee:Employee): Observable<Employee> {
+        return this.http.post<Employee>('${this.apiServerUrl}/employee/add',employee);
         
     }
 }
